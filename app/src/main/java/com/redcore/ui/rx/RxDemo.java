3ad369da61c06fc6a2d43b1f4d066195cd9f7e06
zm <javaczhang@gmail.com>
2019-02-12 23:46:47 +0800

    rx demos

--- app/src/main/java/com/redcore/ui/rx/RxDemo.java
+++ app/src/main/java/com/redcore/ui/rx/RxDemo.java
@@ -1,9 +1,47 @@
 package com.redcore.ui.rx;
 
-public class RxDemo {
-    public static  void main(String[] args){
+import android.util.Log;
 
+import java.util.concurrent.TimeUnit;
+
+import io.reactivex.Observable;
+import io.reactivex.Observer;
+import io.reactivex.disposables.Disposable;
+
+public class RxDemo {
+
+    static  String TAG ="rx";
+    public static  void test(){
+        Observable.just(1, 2, 3)
+                .delay(3, TimeUnit.SECONDS) // 延迟3s再发送，由于使用类似，所以此处不作全部展示
+                .subscribe(new Observer<Integer>() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
+
+                    }
+
+                    @Override
+                    public void onNext(Integer value) {
+                        Log.d(TAG, "接收到了事件"+ value  );
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        Log.d(TAG, "对Error事件作出响应");
+                    }
+
+                    @Override
+                    public void onComplete() {
+                        Log.d(TAG, "对Complete事件作出响应");
+                    }
+                });
 
         System.out.println("hi");
+
+
+    }
+
+    public static  void main(String[] args){
+test();
     }
 }
