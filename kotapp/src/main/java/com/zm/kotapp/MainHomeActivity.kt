9fc08ba3b03213fd2868f1c188c7f4d4db0869da
andy <objcjava@163.com>
2020-12-18 13:32:46 +0800

    add  tabledActivity

--- kotapp/src/main/java/com/zm/kotapp/MainHomeActivity.kt
+++ kotapp/src/main/java/com/zm/kotapp/MainHomeActivity.kt
@@ -1,8 +1,10 @@
 package com.zm.kotapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
-import android.widget.Toast
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main_home.*
 
 class MainHomeActivity : AppCompatActivity() {
 
@@ -10,5 +12,13 @@ class MainHomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_home)
 //        Toast.makeText(this, "每日头条" + 12, Toast.LENGTH_SHORT).show();
+        getUser.setText("好好好")
+        getUser.setOnClickListener(object : View.OnClickListener {
+               override fun onClick(v: View?) {
+//                   Toast.makeText(MainHomeActivity.this, "每日头条" + 12, Toast.LENGTH_SHORT).show();
+//                   startActivity(Intent(this, MerchantCenterActivity::class.java))
+                    }
+             })
+        removeUser.setText("好")
     }
 }
