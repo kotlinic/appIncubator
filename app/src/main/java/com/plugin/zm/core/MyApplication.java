7496f8a82857a7aac8cbeddbe52125869d265807
andy <objcjava@163.com>
2020-12-20 00:10:15 +0800

    init 可作为as3.6项目模板

---
+++ app/src/main/java/com/plugin/zm/core/MyApplication.java
@@ -0,0 +1,73 @@
+package com.plugin.zm.core;
+
+import android.app.Application;
+import android.content.ComponentCallbacks;
+import android.content.Context;
+import android.content.res.Configuration;
+
+public class MyApplication extends Application {
+
+    public MyApplication() {
+        super();
+    }
+
+    @Override
+    public void attachBaseContext(Context context) {
+        super.attachBaseContext(context);
+    }
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        // 在主进程初始化调用哈
+    }
+
+    @Override
+    public void onTerminate() {
+        super.onTerminate();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+    }
+
+    @Override
+    public void onTrimMemory(int level) {
+        super.onTrimMemory(level);
+    }
+
+    @Override
+    public void registerComponentCallbacks(ComponentCallbacks callback) {
+        super.registerComponentCallbacks(callback);
+    }
+
+    @Override
+    public void unregisterComponentCallbacks(ComponentCallbacks callback) {
+        super.unregisterComponentCallbacks(callback);
+    }
+
+    @Override
+    public void registerActivityLifecycleCallbacks(ActivityLifecycleCallbacks callback) {
+        super.registerActivityLifecycleCallbacks(callback);
+    }
+
+    @Override
+    public void unregisterActivityLifecycleCallbacks(ActivityLifecycleCallbacks callback) {
+        super.unregisterActivityLifecycleCallbacks(callback);
+    }
+
+    @Override
+    public void registerOnProvideAssistDataListener(OnProvideAssistDataListener callback) {
+        super.registerOnProvideAssistDataListener(callback);
+    }
+
+    @Override
+    public void unregisterOnProvideAssistDataListener(OnProvideAssistDataListener callback) {
+        super.unregisterOnProvideAssistDataListener(callback);
+    }
+}
