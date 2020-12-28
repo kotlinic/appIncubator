81eb11d75c8503918fc977840ed85dfb2a8993ca
andy <objcjava@163.com>
2020-12-29 00:44:47 +0800

    listview in tab

--- lib/pages/Fourth.dart
+++ lib/pages/Fourth.dart
@@ -1,19 +1,37 @@
 import 'package:flutter/material.dart';
+
 class Fourth extends StatefulWidget {
   @override
-  _FourthState createState() => _FourthState();
+  _FourthState createState() => _FourthState(
+        items: new List<String>.generate(
+            10000, (i) => "Flutter实践中遇到的部分问题[持续更新]$i"),
+      );
 }
 
 class _FourthState extends State<Fourth> {
-    @override
-    Widget build(BuildContext context) {
-      return new Scaffold(
+  final List<String> items;
+
+  _FourthState({Key key, @required this.items});
+
+  @override
+  Widget build(BuildContext context) {
+    final title = 'Long List';
+
+    return new MaterialApp(
+      title: title,
+      home: new Scaffold(
         appBar: new AppBar(
-          title: new Text('第四个界面'),
+          title: new Text(title),
         ),
-        body: new Center(
-          child: new Text('Fourth'),
+        body: new ListView.builder(
+          itemCount: items.length,
+          itemBuilder: (context, index) {
+            return new ListTile(
+              title: new Text('${items[index]}'),
+            );
+          },
         ),
-      );;
-    }
+      ),
+    );
+  }
 }
